5c20196a40d7346a71ce4b556fe35daea2d768fd
morph027 <morphsen@gmx.com>
2017-03-03 12:34:40 +0100

    Update officeonline-install.sh

--- officeonline-install.sh
+++ officeonline-install.sh
@@ -10,21 +10,23 @@
 
 if [[ `id -u` -ne 0 ]] ; then echo 'Please run me as root or "sudo ./officeonline-install.sh"' ; exit 1 ; fi
 
+randpass() {
+  [ "$2" == "0" ] && CHAR="[:alnum:]" || CHAR="[:graph:]"
+  cat /dev/urandom 2>/dev/null | tr -cd "$CHAR" 2>/dev/null | head -c ${1:-32}
+}
+
 clear
 
 soli="/etc/apt/sources.list"
 log_file="/tmp/officeonline.log"
-ood=libreoffice-5.3.0.3
+ood="libreoffice-5.3.0.3"
 ooo="/opt/libreoffice"
-poco="/opt/poco-1.7.8-all"
-getpoko=poco-1.7.8-all.tar.gz
 oo="/opt/online"
-cpu=`nproc`
+cpu=$(nproc)
 maxcon=200
 maxdoc=100
 
-apt-get update && apt-get install dialog
-
+apt-get update && apt-get -y install dialog
 
 dialog --backtitle "Information" \
 --title "Note" \
@@ -36,28 +38,24 @@ sed -i 's/# deb-src/deb-src/g' $soli
 
 apt-get update && apt-get upgrade -y
 
-apt-get install sudo libegl1-mesa-dev libkrb5-dev systemd python-polib git libkrb5-dev make openssl g++ libtool ccache libpng12-0 libpng12-dev libpcap0.8 libpcap0.8-dev libcunit1 libcunit1-dev libpng12-dev libcap-dev libtool m4 automake libcppunit-dev libcppunit-doc pkg-config npm wget nodejs-legacy libfontconfig1-dev  -y && sudo apt-get build-dep libreoffice -y
+apt-get install sudo curl libegl1-mesa-dev libkrb5-dev systemd python-polib git libkrb5-dev make openssl g++ libtool ccache libpng12-0 libpng12-dev libpcap0.8 libpcap0.8-dev libcunit1 libcunit1-dev libpng12-dev libcap-dev libtool m4 automake libcppunit-dev libcppunit-doc pkg-config npm wget nodejs-legacy libfontconfig1-dev  -y && sudo apt-get build-dep libreoffice -y
 
-useradd lool -G sudo
-mkdir /home/lool
+getent passwd lool || (useradd lool -G sudo; mkdir /home/lool)
 chown lool:lool /home/lool -R
 
-
-wget http://download.documentfoundation.org/libreoffice/src/5.3.0/${ood}.tar.xz -P /opt/
+[ -f /opt/${ood}.tar.xz ] || wget http://download.documentfoundation.org/libreoffice/src/5.3.0/${ood}.tar.xz -P /opt/
 tar xf /opt/${ood}.tar.xz -C  /opt/
 mv /opt/$ood $ooo
 
 chown lool:lool $ooo -R
 
-
-
 sudo -H -u lool bash -c "for dir in ./ ; do (cd "$ooo" && $ooo/autogen.sh --without-help --without-myspell-dicts); done" | tee -a $log_file
 sudo -H -u lool bash -c "for dir in ./ ; do (cd "$ooo" && make); done" | tee -a $log_file
 
-
-######TODO###### We need autocheck last version of pocp and link it to $getpoko   
-wget https://pocoproject.org/releases/poco-1.7.8/$getpoko -P /opt/
-tar xf /opt/$getpoko -C  /opt/
+poco_version=$(curl -s https://pocoproject.org/ | grep -oiE 'The latest stable release is [0-9+]\.[0-9\.]{1,}[0-9]{1,}' | awk '{print $NF}')
+poco="/opt/poco-${poco_version}-all"
+[ -f /opt/${poco_version}-all.tar.gz ] || wget https://pocoproject.org/releases/poco-${poco_version}/${poco_version}-all.tar.gz -P /opt/
+tar xf /opt/${poco_version}-all.tar.gz -C  /opt/
 chown lool:lool $poco -R
 
 sudo -H -u lool bash -c "for dir in ./ ; do (cd "$poco" && ./configure); done" | tee -a $log_file
@@ -65,7 +63,6 @@ sudo -H -u lool bash -c  "for dir in ./ ; do (cd "$poco" && make -j$cpu); done" 
 for dir in ./ ; do (cd "$poco" && make install); done | tee -a $log_file
 
 ###############################################################################
-
 
 git clone https://github.com/husisusi/online $oo
 
@@ -78,13 +75,14 @@ for dir in ./ ; do (cd "$oo" && npm install -g jake); done
 for dir in ./ ; do ( cd "$oo" && ./configure --enable-silent-rules --with-lokit-path=/opt/online/bundled/include --with-lo-path=/opt/libreoffice/instdir --with-max-connections=$maxcon --with-max-documents=$maxdoc --with-poco-includes=/usr/local/include --with-poco-libs=/usr/local/lib --enable-debug && make -j$cpu --directory=$oo); done | tee -a $log_file
 for dir in ./ ; do ( cd "$oo" && make install); done | tee -a $log_file
 
-
 echo "%lool ALL=NOPASSWD:ALL" >> /etc/sudoers
 
 chown -R lool:lool {$ooo,$poco,$oo}
 
 mkdir -p /usr/local/var/cache/loolwsd
 chown -R lool:lool /usr/local/var/cache/loolwsd
+
+PASSWORD=$(randpass 10 0)
 
 cat <<EOT > /lib/systemd/system/loolwsd.service
 
@@ -94,14 +92,13 @@ After=network.target
 
 [Service]
 EnvironmentFile=-/etc/sysconfig/loolwsd
-ExecStart=/opt/online/loolwsd --o:sys_template_path=/opt/online/systemplate --o:lo_template_path=/opt/libreoffice/instdir  --o:child_root_path=/opt/online/jails --o:storage.filesystem[@allow]=true --o:admin_console.username=admin --o:admin_console.password=office1234
+ExecStart=/opt/online/loolwsd --o:sys_template_path=/opt/online/systemplate --o:lo_template_path=/opt/libreoffice/instdir  --o:child_root_path=/opt/online/jails --o:storage.filesystem[@allow]=true --o:admin_console.username=admin --o:admin_console.password="$PASSWORD"
 User=lool
 KillMode=control-group
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
-
 EOT
 
 mkdir /etc/loolwsd
@@ -113,14 +110,11 @@ openssl x509 -req -days 365 -in /etc/loolwsd/cert.csr -signkey /etc/loolwsd/key.
 ln /lib/systemd/system/loolwsd.service /etc/systemd/system/loolwsd.service
 systemctl enable loolwsd.service
 
-
-
 dialog --backtitle "Information" \
 --title "Note" \
---msgbox 'The installation log file is in /tmp/officeonlin.log. After reboot you can use loolwsd.service using: systemctl (start,stop or status) loolwsd.service.
-Your user is admin and password is office1234. Please change your user and/or password in (/lib/systemd/system/loolwsd.service),
+--msgbox 'The installation log file is in /tmp/officeonline.log. After reboot you can use loolwsd.service using: systemctl (start,stop or status) loolwsd.service.
+Your user is admin and password is "$PASSWORD". Please change your user and/or password in (/lib/systemd/system/loolwsd.service),
 after that run (systemctl daemon-reload && systemctl restart loolwsd.service). Please press OK and wait 15 sec. I will start the service.' 10 145
-
 
 clear
 
@@ -133,4 +127,3 @@ ps -ef | grep loolwsd | grep -v grep
 [ $?  -eq "0" ] && echo -e "\033[33;7m### loolwsd is running. Enjoy!!! ###\033[0m" || echo -e "\033[33;5m### loolwsd is not running. Something went wrong :| Please look in /tmp/officeonline.log ###\033[0m"
 lsof -i :9980
 exit
-
