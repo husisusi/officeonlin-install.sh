9ade15e3b58599dfaf33e1940462d74b6f3912fd
Aalaesar <aalaesar@gmail.com>
2017-03-21 22:29:42 +0100

    Fixed LO configuration failure on ubuntu 16.04
    + added exit conditions when configure or make fail

--- officeonline-install.sh
+++ officeonline-install.sh
@@ -93,9 +93,12 @@ if [ ! -d ${lo_dir}/instdir ] || ${lo_forcebuild}; then
 "SO BE PATIENT PLEASE! ! You may see errors during the installation, just ignore them and let it do the work." 10 78
     clear
   fi
-  sudo -u lool bash -c "cd ${lo_dir} && ./autogen.sh --without-help --without-myspell-dicts" | tee -a $log_file
+  cd ${lo_dir}
+  sudo -Hu lool ./autogen.sh --without-help --without-myspell-dicts | tee -a $log_file
+  [ ${PIPESTATUS[0]} -ne 0 ] && exit 2
   # libreoffice take around 8/${cpu} hours to compile on fast cpu.
-  sudo -u lool bash -c "cd ${lo_dir} && make" | tee -a $log_file
+  sudo -Hu lool make | tee -a $log_file
+  [ ${PIPESTATUS[0]} -ne 0 ] && exit 2
 fi
 
 ###############################################################################
@@ -116,10 +119,13 @@ fi
 # so let say arbitrary : do compilation when folder size is less than 100Mo
 if [ $(du -s ${poco} | awk '{print $1}') -lt 100000 ]; then
   cd "$poco"
-  sudo -u lool ./configure | tee -a $log_file
-  sudo -u lool make -j${cpu} | tee -a $log_file
+  sudo -Hu lool ./configure | tee -a $log_file
+  [ ${PIPESTATUS[0]} -ne 0 ] && exit 3
+  sudo -Hu lool make -j${cpu} | tee -a $log_file
+  [ ${PIPESTATUS[0]} -ne 0 ] && exit 3
   # poco take around 22/${cpu} minutes to compile on fast cpu
   make install | tee -a $log_file
+  [ ${PIPESTATUS[0]} -ne 0 ] && exit 3
 fi
 ###############################################################################
 ########################### loolwsd Installation ##############################
@@ -156,12 +162,13 @@ if [ -f /etc/sudoers ] && ! grep -q 'lool' /etc/sudoers; then
   fi
 fi
 cd ${lool_dir}
-[ -f ${lool_dir}/loolwsd ] && sudo -u lool make clean
-sudo -u lool ./autogen.sh
+[ -f ${lool_dir}/loolwsd ] && sudo -Hu lool make clean
+sudo -Hu lool ./autogen.sh
 [ -n "${lool_logfile}" ] && lool_configure_opts="${lool_configure_opts} --with-logfile=${lool_logfile}"
-sudo -u lool bash -c "./configure --enable-silent-rules --with-lokit-path=${lool_dir}/bundled/include --with-lo-path=${lo_dir}/instdir --with-max-connections=$lool_maxcon --with-max-documents=$lool_maxdoc --with-poco-includes=/usr/local/include --with-poco-libs=/usr/local/lib ${lool_configure_opts}" | tee -a $log_file
+sudo -Hu lool bash -c "./configure --enable-silent-rules --with-lokit-path=${lool_dir}/bundled/include --with-lo-path=${lo_dir}/instdir --with-max-connections=$lool_maxcon --with-max-documents=$lool_maxdoc --with-poco-includes=/usr/local/include --with-poco-libs=/usr/local/lib ${lool_configure_opts}" | tee -a $log_file
+[ ${PIPESTATUS[0]} -ne 0 ] && exit 4
 # loolwsd+loleaflet take around 8.5/${cpu} minutes to compile on fast cpu
-sudo -u lool bash -c "make -j$cpu --directory=${lool_dir}" | tee -a $log_file
+sudo -Hu lool make -j$cpu --directory=${lool_dir} | tee -a $log_file
 _loolwsd_make_rc=${?} # get the make return code
 ### remove lool group from sudoers
 if [ -f /etc/sudoers ]; then
@@ -220,7 +227,7 @@ if [ ${sh_interactive} ]; then
 "after that run (systemctl daemon-reload && systemctl restart loolwsd.service).\nPlease press OK and wait 15 sec. I will start the service." 10 145
   clear
 fi
-sudo -u lool bash -c "${lool_dir}/loolwsd --o:sys_template_path=${lool_dir}/systemplate --o:lo_template_path=${lo_dir}/instdir  --o:child_root_path=${lool_dir}/jails --o:storage.filesystem[@allow]=true --o:admin_console.username=admin --o:admin_console.password=admin &"
+sudo -Hu lool bash -c "${lool_dir}/loolwsd --o:sys_template_path=${lool_dir}/systemplate --o:lo_template_path=${lo_dir}/instdir  --o:child_root_path=${lool_dir}/jails --o:storage.filesystem[@allow]=true --o:admin_console.username=admin --o:admin_console.password=admin &"
 rm -rf ${lo_dir}/workdir
 sleep 10
 ps -u lool | grep loolwsd
