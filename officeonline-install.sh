0f14399d5d58ff126e85abc51a451af29a235612
Aalaesar <aalaesar@gmail.com>
2017-03-23 15:43:33 +0100

    set version to 2.1.0

--- officeonline-install.sh
+++ officeonline-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#VERSION 2.0.1
+#VERSION 2.1.0
 #Written by: Subhi H. & Marc C.
 #Github Contributors: Aalaesar, Kassiematis, morph027
 #This script is free software: you can redistribute it and/or modify it under
