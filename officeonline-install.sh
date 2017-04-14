3a8e1050bc72d63edf0bea6143857f4ddefe539a
Sergey Dryabzhinsky <sergey@rusoft.ru>
2017-04-14 03:29:14 +0300

    System apr mean system serf

--- officeonline-install.sh
+++ officeonline-install.sh
@@ -268,7 +268,7 @@ apt-get install sudo curl libkrb5-dev systemd python-polib git dpkg-dev make ope
  libboost-dev libboost-date-time-dev libboost-iostreams-dev libboost-system-dev libboost-program-options-dev libboost-filesystem-dev \
  libjemalloc-dev libeot-dev libcunit1 libcunit1-dev libcap-dev libcppunit-dev libfontconfig1-dev libexpat-dev \
  libpoppler-private-dev libpoppler-cpp-dev libharfbuzz-dev libcairo2-dev libapr1-dev libaprutil1-dev libldap2-dev \
- libgl1-mesa-dev libxt-dev x11proto-render-dev libx11-dev libxrandr-dev libxrender-dev libpq-dev \
+ libgl1-mesa-dev libxt-dev x11proto-render-dev libx11-dev libxrandr-dev libxrender-dev libpq-dev libserf-dev \
  libxslt1-dev xsltproc libxml2-utils uuid-runtime gperf -y
 [ $? -ne 0 ] && exit 1
 apt-get build-dep libreoffice -y
@@ -363,6 +363,7 @@ if [ ! -d ${lo_dir}/instdir ] || ${lo_forcebuild}; then
     --with-system-cairo \
     --with-system-apr \
     --with-system-curl \
+    --with-system-serf \
     --with-system-openssl \
     --with-system-openldap \
     --with-system-postgresql \
