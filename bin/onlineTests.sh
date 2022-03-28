34439e3e1ee37e58b881a4232ff062c63c1546ea
DmDS <dmds@mail.ru>
2022-03-28 07:39:27 +0300

    Update onlineTests.sh

--- bin/onlineTests.sh
+++ bin/onlineTests.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # shellcheck disable=SC2154
 # tests script
-# sudo -Hu cool bash -c "coolwsd --o:sys_template_path=${cool_dir}/systemplate --o:lo_template_path=${lo_dir}/instdir --o:child_root_path=${cool_dir}/jails --o:admin_console.username=admin --o:admin_console.password=admin &"
 systemctl start $coolwsd_service_name.service
 rm -rf ${lo_dir}/workdir
 sleep 18
 if pgrep -u cool coolwsd; then
+  clear
   echo -e "\033[33;7m### coolwsd is running. Enjoy!!! ###\033[0m"
   lsof -i :9980
-  pkill -u cool coolwsd
   systemctl enable $coolwsd_service_name.service
   systemctl daemon-reload
 else
