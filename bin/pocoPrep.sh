99b7e511a493d1e6c660e7a2d556a975c46039b3
DmDS <dmds@mail.ru>
2023-11-11 16:14:56 +0300

    Update pocoPrep.sh
    poco ver url fix

--- bin/pocoPrep.sh
+++ bin/pocoPrep.sh
@@ -6,9 +6,9 @@
 ## Download & install Poco Sources
 [ -z "$poco_version" ] && poco_version=$poco_version_latest
 [ -z "$poco_dir" ] && poco_dir="/opt/poco-${poco_version}-all"
-poco_version_folder=$(grep -oiE '[0-9+]\.[0-9\.]{1,}[0-9]{1,}' <<<"${poco_version}")
+#poco_version_folder=$(grep -oiE '[0-9+]\.[0-9\.]{1,}[0-9]{1,}' <<<"${poco_version}")
 if [ ! -d $poco_dir ]; then
-  wget -c https://pocoproject.org/releases/poco-${poco_version_folder}/poco-${poco_version}-all.tar.gz -P "$(dirname $poco_dir)"/ || exit 3
+  wget -c https://pocoproject.org/releases/poco-${poco_version}/poco-${poco_version}-all.tar.gz -P "$(dirname $poco_dir)"/ || exit 3
   tar xf "$(dirname $poco_dir)"/poco-${poco_version}-all.tar.gz -C  "$(dirname $poco_dir)"/
   rm -f poco*.tar.gz
   chown cool:cool $poco_dir -R
