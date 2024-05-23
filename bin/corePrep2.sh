046d257760d51c097cb05f323cb173c3f8b361ed
DmDS <dmds@mail.ru>
2024-05-23 09:17:42 +0300

    core-co-24.04-assets

--- bin/corePrep2.sh
+++ bin/corePrep2.sh
@@ -8,10 +8,10 @@ echo
 echo "Downloading Libre Office core assets (this might take a while) ..."
 mkdir ${lo_dir}
 cd ${lo_dir}
-wget https://github.com/CollaboraOnline/online/releases/download/for-code-assets/core-co-23.05-assets.tar.gz -q --show-progress --progress=bar:force
+wget https://github.com/CollaboraOnline/online/releases/download/for-code-assets/core-co-24.04-assets.tar.gz -q --show-progress --progress=bar:force
 chown -R cool:cool ${lo_dir}
 echo
 echo "Unpacking ..."
-tar xf core-co-23.05-assets.tar.gz
-rm core-co-23.05-assets.tar.gz
+tar xf core-co-24.04-assets.tar.gz
+rm core-co-24.04-assets.tar.gz
 set +e
